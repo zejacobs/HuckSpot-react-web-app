def6ff6f361ec87e3e983c568d081890d68625be
zejacobs <jacobs.za@northeastern.edu>
2024-04-24 16:17:45 -0400

    fixed side bar vertical fill

--- src/HuckSpot/index.tsx
+++ src/HuckSpot/index.tsx
@@ -17,26 +17,24 @@ function HuckSpot() {
       <div className="flex-row">
         <Navigation />
       </div>
-      <div className="flex-row main-content">
-        <div className=" flex-row d-flex">
-          <div className="d-none d-lg-block side-bar"></div>
-          <div className="flex-fill container p-4 ">
-            <Routes>
-              <Route path="/" element={<Navigate to="Home" />} />
-              <Route path="/Home" element={<Home />} />
-              <Route path="/Login" element={<Login />} />
-              <Route path="/Register" element={<Register />} />
-              <Route path="/Profile/" element={<Profile />} />
-              <Route path="/Profile/:userId" element={<Profile />} />
-              <Route path="/Search" element={<Search />} />
-              <Route path="/Search/:searchType" element={<SearchResults />} />
-              <Route path="/Details/Discs/:discId" element={<DiscDetails />} />
-              <Route path="/Details/Tournaments/:tournamentId" element={<TournamentDetails />} />
-              <Route path="/Admin/:adminType" element={<Admin />} />
-            </Routes>
-          </div>
-          <div className="d-none d-lg-block side-bar"></div>
+      <div className=" flex-row main-content">
+        <div className="d-none d-lg-block side-bar"></div>
+        <div className="flex-fill container p-4 ">
+          <Routes>
+            <Route path="/" element={<Navigate to="Home" />} />
+            <Route path="/Home" element={<Home />} />
+            <Route path="/Login" element={<Login />} />
+            <Route path="/Register" element={<Register />} />
+            <Route path="/Profile/" element={<Profile />} />
+            <Route path="/Profile/:userId" element={<Profile />} />
+            <Route path="/Search" element={<Search />} />
+            <Route path="/Search/:searchType" element={<SearchResults />} />
+            <Route path="/Details/Discs/:discId" element={<DiscDetails />} />
+            <Route path="/Details/Tournaments/:tournamentId" element={<TournamentDetails />} />
+            <Route path="/Admin/:adminType" element={<Admin />} />
+          </Routes>
         </div>
+        <div className="d-none d-lg-block side-bar"></div>
       </div>
     </div>
   );
